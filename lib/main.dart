45cd11cabc8bd9ec6e2582e97cf8587ebd878f32
Belicio Batista Cardoso <belloinfo@gmail.com>
2019-10-30 13:25:02 -0300

    teste

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     
     return MaterialApp(
-      title: 'Bem ao Aplicativo',
+      title: 'Bem vindo ao Aplicativo',
       theme: ThemeData(
         primarySwatch: Colors.indigo,
       ),
