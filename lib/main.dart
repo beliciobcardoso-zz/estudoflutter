e0a3aba2f80fda58ae8e3f14fdd2077504e5bc56
Belicio Batista Cardoso <belloinfo@gmail.com>
2019-10-31 13:42:18 -0300

    fim da 1 parte

--- lib/main.dart
+++ lib/main.dart
@@ -6,31 +6,74 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    
     return MaterialApp(
-      title: 'Bem vindo ao Aplicativo',
+      title: 'Gerador de Nomes StartUp',
       theme: ThemeData(
         primarySwatch: Colors.indigo,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Bem vindo ao Flutter'),
-        ),
-        body: Center(
-          //child: Text('Hello World'),
-          //child: Text(wordPair.asPascalCase),
-          child: RandomWords(),
-        ),
-      ),
+      home: RandomWords(),
     );
   }
 }
 
 class RandomWordsState extends State<RandomWords> {
+  final List<WordPair> _suggestions = <WordPair>[];
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18);
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16),
+        // The itemBuilder callback is called once per suggested
+        // word pairing, and places each suggestion into a ListTile
+        // row. For even rows, the function adds a ListTile row for
+        // the word pairing. For odd rows, the function adds a
+        // Divider widget to visually separate the entries. Note that
+        // the divider may be difficult to see on smaller devices.
+        itemBuilder: (BuildContext _context, int i) {
+          // Add a one-pixel-high divider widget before each row
+          // in the ListView.
+          if (i.isOdd) {
+            return Divider();
+          }
+
+          // A sintaxe "i ~ / 2" divide i por 2 e retorna um
+          // resultado inteiro.
+          // Por exemplo: 1, 2, 3, 4, 5 se torna 0, 1, 1, 2, 2.
+          // Calcula o número real de pares de palavras
+          // no ListView, menos os widgets divisores.
+          final int index = i ~/ 2;
+          // If you've reached the end of the available word
+          // pairings...
+          if (index >= _suggestions.length) {
+            // ...then generate 10 more and add them to the
+            // suggestions list.
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+
   @override
-  Widget build(BuildContext context){
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+  Widget build(BuildContext context) {
+    //final wordPair = WordPair.random();
+    //return Text(wordPair.asPascalCase);
+
+    return Scaffold (                   // Add from here... 
+      appBar: AppBar(
+        title: Text('Gerador de Nomes StartUp'),
+      ),
+      body: _buildSuggestions(),
+    );     
   }
 }
 
