6d9b5f274c88b08739f3802c067d77afff6a9d0f
Belicio Batista Cardoso <44432451+belloinfo@users.noreply.github.com>
2019-11-02 08:03:45 -0300

    finalizado - 6. Navigate to a new screen

--- lib/main.dart
+++ lib/main.dart
@@ -18,17 +18,18 @@ class MyApp extends StatelessWidget {
 
 class RandomWordsState extends State<RandomWords> {
   final List<WordPair> _suggestions = <WordPair>[];
+  final Set<WordPair> _saved = Set<WordPair>();
   final TextStyle _biggerFont = const TextStyle(fontSize: 18);
 
   Widget _buildSuggestions() {
     return ListView.builder(
         padding: const EdgeInsets.all(16),
-        // The itemBuilder callback is called once per suggested
-        // word pairing, and places each suggestion into a ListTile
-        // row. For even rows, the function adds a ListTile row for
-        // the word pairing. For odd rows, the function adds a
-        // Divider widget to visually separate the entries. Note that
-        // the divider may be difficult to see on smaller devices.
+        // O retorno de chamada itemBuilder é chamado uma vez por sugestão
+        // emparelhamento de palavras e coloca cada sugestão em um ListTile
+        // linha. Para linhas pares, a função adiciona uma linha ListTile para
+        // a palavra emparelhamento. Para linhas ímpares, a função adiciona um
+        // Widget divisor para separar visualmente as entradas. Observe que
+        // pode ser difícil ver o divisor em dispositivos menores.
         itemBuilder: (BuildContext _context, int i) {
           // Add a one-pixel-high divider widget before each row
           // in the ListView.
@@ -54,26 +55,68 @@ class RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _saved.contains(pair);
     return ListTile(
-
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    //final wordPair = WordPair.random();
-    //return Text(wordPair.asPascalCase);
-
-    return Scaffold (                   // Add from here... 
+    return Scaffold(
       appBar: AppBar(
         title: Text('Gerador de Nomes StartUp'),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.view_list), onPressed: _pushSaved),
+        ],
       ),
       body: _buildSuggestions(),
-    );     
+    );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          final Iterable<ListTile> tiles = _saved.map(
+            (WordPair pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final List<Widget> divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList();
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        },
+      ),
+    );
   }
 }
 
